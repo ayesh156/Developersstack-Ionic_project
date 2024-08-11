e5d990523fe34d2005a82a918324691ac2125014
Ayesh Chathuranga <92289527+ayesh156@users.noreply.github.com>
2024-08-11 13:18:16 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig : {
     apiKey: "AIzaSyDA911DCNS1cZ1yLaVNEWNszPvBS58Zv5w",
     authDomain: "ionic-app-551d8.firebaseapp.com",
