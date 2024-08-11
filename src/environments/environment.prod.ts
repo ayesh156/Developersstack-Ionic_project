c02df2a59157e35c10aa8b39bc36b69df273b8fb
= <=>
2024-08-11 13:08:36 +0530

    Fix issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: false
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDA911DCNS1cZ1yLaVNEWNszPvBS58Zv5w",
+    authDomain: "ionic-app-551d8.firebaseapp.com",
+    projectId: "ionic-app-551d8",
+    storageBucket: "ionic-app-551d8.appspot.com",
+    messagingSenderId: "387175996351",
+    appId: "1:387175996351:web:9568e20732d9be03cc1f0a",
+    measurementId: "G-E95LD8M0PB"
+  }
 };
